de9dfffa02796044df4d1efe2ba7627c245ec02a
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2023-11-15 12:38:27 +0100

    add horner rule algorithm

---
+++ math/HornerAlgorithm.java
@@ -0,0 +1,18 @@
+package math;
+
+class HornerAlgorithm {
+    public static void main(String[] args) {
+        int p[] = { -1, 0, 2, 0, 3, 5 };
+        int x = -2;
+
+        System.out.println(hornerRule(p, x));
+    }
+
+    // How it works: https://www.youtube.com/watch?v=Z393AcN_Gz0
+    static int hornerRule(int[] p, int x) {
+        int result = p[0];
+        for (int i = 1; i < p.length; i++)
+            result = p[i] + (x * result);
+        return result;
+    }
+}
