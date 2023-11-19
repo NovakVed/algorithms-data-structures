f310b3b24839fd712b326592bfc432254668a57b
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2023-11-20 00:28:06 +0100

    add package to Increments.java

--- temp/Increments.java
+++ temp/Increments.java
@@ -1,10 +1,12 @@
+package temp;
+
 class Increments {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int i = 0;
-        System.out.println("i++: " + i++);
-        System.out.println("i: " + i);
-        System.out.println("++i: " + ++i);
-        System.out.println("i++: " + i++);
-        System.out.println("i+1: " + (i+1));
+        System.out.println("i++: " + i++); // still 0
+        System.out.println("i: " + i); // 1
+        System.out.println("++i: " + ++i); // 2
+        System.out.println("i++: " + i++); // 2
+        System.out.println("i+1: " + (i + 1)); // 4
     }
 }
