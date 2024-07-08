ffbe6480eee8cb812fbe0970f8d1417aed5b5b04
Vedran Novak <vednovak@gmail.com>
2024-07-08 20:51:35 +0200

    add Temp.java

---
+++ temp/Temp.java
@@ -0,0 +1,28 @@
+package temp;
+
+public class Temp {
+    public static void main(String[] args) {
+        // int[] arr = new int[] { 1, 2 };
+        // arr[0] = arr[0] + arr[1];
+        // arr[1] = arr[0] - arr[1];
+        // arr[0] = arr[0] - arr[1];
+
+        // arr[0] ^= arr[1];
+        // arr[1] ^= arr[0];
+        // arr[0] ^= arr[1];
+
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.println(arr[i]);
+        // }
+
+        // // int i = Integer.MAX_VALUE;
+        // int j = Integer.MAX_VALUE;
+
+        // System.out.println(i);
+        // System.out.println(i + j);
+
+        System.out.println(3 / 2);
+
+    }
+
+}
