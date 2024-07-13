a05eee95288ba87013b63a265dd9ceb3107909fa
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-07-13 14:12:04 +0200

    refactor GreedySetCover.java

--- algorithms/greedyAlgorithm/GreedySetCover.java
+++ algorithms/greedyAlgorithm/GreedySetCover.java
@@ -1,59 +1,50 @@
 package algorithms.greedyAlgorithm;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class GreedySetCover {
 
     public static void main(String[] args) {
-        List<Set<String>> stations = new ArrayList<>();
-        stations.add(new HashSet<>(Arrays.asList("ID", "NV", "UT")));
-        stations.add(new HashSet<>(Arrays.asList("WA", "ID", "MT")));
-        stations.add(new HashSet<>(Arrays.asList("OR", "NV", "CA")));
-        stations.add(new HashSet<>(Arrays.asList("NV", "UT")));
-        stations.add(new HashSet<>(Arrays.asList("CA", "AZ")));
+        Set<String> allStates = new HashSet<>(Arrays.asList("mt", "wa", "or", "id", "nv", "ut", "ca", "az"));
+        Map<String, Set<String>> stations = new LinkedHashMap<>();
 
-        // List of all states that need to be covered
-        Set<String> allStates = new HashSet<>(Arrays.asList(
-                "ID", "NV", "UT", "WA", "MT", "OR", "CA", "AZ"));
+        stations.put("kone", new HashSet<>(Arrays.asList("id", "nv", "ut")));
+        stations.put("ktwo", new HashSet<>(Arrays.asList("wa", "id", "mt")));
+        stations.put("kthree", new HashSet<>(Arrays.asList("or", "nv", "ca")));
+        stations.put("kfour", new HashSet<>(Arrays.asList("nv", "ut")));
+        stations.put("kfive", new HashSet<>(Arrays.asList("ca", "az")));
 
         // Find the minimum set of stations using the greedy algorithm
         // prints it!
-        findMinimumCover(stations, allStates)
-                .stream()
-                .flatMap(Set::stream)
-                .forEach(System.out::println);
+        findMinimumCover(stations, allStates).stream().forEach(System.out::println);
     }
 
     // Greedy algorithm to find the minimum set of stations that covers all states
-    public static Set<Set<String>> findMinimumCover(List<Set<String>> stations, Set<String> allStates) {
-        Set<Set<String>> finalSolution = new HashSet<>();
-        Set<String> coveredStates = new HashSet<>();
-        while (!coveredStates.containsAll(allStates)) {
-            Set<String> bestStation = null;
-            int bestStationCoveredCount = 0;
+    public static Set<String> findMinimumCover(Map<String, Set<String>> stations, Set<String> allStates) {
+        Set<String> finalStations = new HashSet<String>();
+        while (!allStates.isEmpty()) {
+            String bestStation = null;
+            Set<String> statesCovered = new HashSet<>();
 
-            for (Set<String> station : stations) {
-                Set<String> covered = new HashSet<>(station);
-                covered.removeAll(coveredStates);
+            for (Map.Entry<String, Set<String>> station : stations.entrySet()) {
+                Set<String> covered = new HashSet<>(allStates);
+                covered.retainAll(station.getValue());
 
-                if (covered.size() > bestStationCoveredCount) {
-                    bestStation = station;
-                    bestStationCoveredCount = covered.size();
+                if (covered.size() > statesCovered.size()) {
+                    bestStation = station.getKey();
+                    statesCovered = covered;
+                }
+                allStates.removeIf(statesCovered::contains);
+
+                if (bestStation != null) {
+                    finalStations.add(bestStation);
                 }
             }
-
-            if (bestStation == null) {
-                return null; // No solution found
-            }
-
-            finalSolution.add(bestStation);
-            coveredStates.addAll(bestStation);
         }
-
-        return finalSolution;
+        return finalStations;
     }
 }
