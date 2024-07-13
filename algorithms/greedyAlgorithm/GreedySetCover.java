f5081503bc709de20731786148725fac000cf655
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-07-13 15:31:25 +0200

    refactor GreedySetCover.java

--- algorithms/greedyAlgorithm/GreedySetCover.java
+++ algorithms/greedyAlgorithm/GreedySetCover.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class GreedySetCover {
 
     public static void main(String[] args) {
-        Set<String> allStates = new HashSet<>(Arrays.asList("mt", "wa", "or", "id", "nv", "ut", "ca", "az"));
+        Set<String> allStates = new HashSet<>(Arrays.asList("mt", "wa", "or", "id", "nv", "ut", "ca", "az", "vn"));
         Map<String, Set<String>> stations = new LinkedHashMap<>();
 
         stations.put("kone", new HashSet<>(Arrays.asList("id", "nv", "ut")));
@@ -18,32 +18,40 @@ public class GreedySetCover {
         stations.put("kfour", new HashSet<>(Arrays.asList("nv", "ut")));
         stations.put("kfive", new HashSet<>(Arrays.asList("ca", "az")));
 
-        // Find the minimum set of stations using the greedy algorithm
         findMinimumCover(stations, allStates).stream().forEach(System.out::println);
     }
 
-    // Greedy algorithm to find the minimum set of stations that covers all states
     public static Set<String> findMinimumCover(Map<String, Set<String>> stations, Set<String> allStates) {
         Set<String> finalStations = new HashSet<String>();
-        while (!allStates.isEmpty()) {
+        Set<String> statesRemaining = new HashSet<>(allStates);
+
+        while (!statesRemaining.isEmpty()) {
             String bestStation = null;
             Set<String> statesCovered = new HashSet<>();
 
             for (Map.Entry<String, Set<String>> station : stations.entrySet()) {
-                Set<String> covered = new HashSet<>(allStates);
+                Set<String> covered = new HashSet<>(statesRemaining);
                 covered.retainAll(station.getValue());
 
                 if (covered.size() > statesCovered.size()) {
                     bestStation = station.getKey();
                     statesCovered = covered;
                 }
-                allStates.removeIf(statesCovered::contains);
-
-                if (bestStation != null) {
-                    finalStations.add(bestStation);
-                }
             }
+
+            if (statesCovered.isEmpty()) {
+                break;
+            }
+
+            statesRemaining.removeAll(statesCovered);
+            finalStations.add(bestStation);
         }
+
+        // Print any states that could not be covered
+        if (!statesRemaining.isEmpty()) {
+            System.out.println("The following states could not be covered: " + statesRemaining);
+        }
+
         return finalStations;
     }
 }
