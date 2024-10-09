cf23e5e1f1f4aeda316d838af7528e509c19e454
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-10-10 00:49:06 +0200

    add explanation EuclidsAlgorithm.java

--- algorithms/math/euclidsAlgorithm/EuclidsAlgorithm.java
+++ algorithms/math/euclidsAlgorithm/EuclidsAlgorithm.java
@@ -3,9 +3,12 @@ class EuclidsAlgorithm {
         System.out.println(greatestCommonDivisor(270, 192));
     }
 
+    // How it works: https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
     private static int greatestCommonDivisor(int a, int b) {
-        if (a == 0) return b;
-        if (b == 0) return a;
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
 
         int q = a / b;
         int r = a - b * q;
