1534c930ec5fe191d3c5fd7eed738f36d31aff73
NovakVed <vednovak@gmail.com>
2024-12-18 15:36:56 +0100

    add FibonacciSequence1 practice

---
+++ algorithms/math/fibonacciAlgorithm/practice/FibonacciSequence1.java
@@ -0,0 +1,16 @@
+package algorithms.math.fibonacciAlgorithm.practice;
+
+public class FibonacciSequence1 {
+
+    // 5! = 5 * 4 * 3 * 2 * 1
+    private static int fibonacci(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * fibonacci(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+    }
+}
