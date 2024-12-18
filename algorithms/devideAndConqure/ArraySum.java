6c260f823f1d55cc0b5723c0633834df834c130c
NovakVed <vednovak@gmail.com>
2024-12-19 00:10:55 +0100

    add arraySum d&c

---
+++ algorithms/devideAndConqure/ArraySum.java
@@ -0,0 +1,14 @@
+package algorithms.devideAndConqure;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        System.out.println(sum(arr, 0));
+    }
+
+    private static int sum(int[] arr, int i) {
+        if (i >= arr.length - 1) return arr[i];
+        return arr[i] + sum(arr, ++i);
+    }
+
+}
