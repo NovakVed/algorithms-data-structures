c3864cc4a395963a571ef4c76f07a3837beb97a6
Vedran Novak <vednovak@gmail.com>
2026-03-13 19:56:18 +0100

    add binary search

---
+++ python/algorithms/search/binary_search.py
@@ -0,0 +1,15 @@
+def solution(list, target):
+    l = 0
+    r = len(list) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if list[m] == target:
+            return True
+        if list[m] > target:
+            r = m - 1
+        else:
+            l = m + 1
+
+    return False
+
+print(solution([1,2,3,4,5,6,7,8,9,10,11,12], 9))
