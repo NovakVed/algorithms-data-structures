76816d4db2ce55cd4ef6394d881d5bbf1c08aebb
Vedran Novak <vednovak@gmail.com>
2026-03-22 15:42:04 +0100

    add selection_sort_1.py

---
+++ python/algorithms/sort/practice/selection_sort_1.py
@@ -0,0 +1,15 @@
+def selectionSort(arr):
+    for i in range(0, len(arr)):
+        smallest = i
+        for j in range(i + 1, len(nums)):
+            if arr[j] < arr[smallest]:
+                smallest = j
+                
+        if i is not smallest:
+            arr[i] ^= arr[smallest]
+            arr[smallest] ^= arr[i]
+            arr[i] ^= arr[smallest]
+    return arr
+
+nums = [2, 5, 8, 3, 9, 4, 1]
+print(selectionSort(nums)) 
