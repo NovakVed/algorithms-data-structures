c354cea51163c80d12f43ee6f36ae0764ea3d4a8
Vedran Novak <vednovak@gmail.com>
2026-03-29 23:08:23 +0200

    add insertionSort practice

--- python/algorithms/sort/insertion_sort/practice/insertion_sort_2.py
+++ python/algorithms/sort/insertion_sort/practice/insertion_sort_2.py
@@ -1,4 +1,11 @@
-# def insertion_sort(arr):
-    
-    
-# print(insertion_sort([5, 4, 2, 3]))
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+print(insertion_sort([4, 5, 2, 3]))
